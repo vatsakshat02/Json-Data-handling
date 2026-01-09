83240a1736acc5c8066ef0d06ce7c3df6c7a2116
vatsakshat02 <vatsakshat02@gmail.com>
2026-01-09 15:05:04 +0530

    feat:removed people with less than 3 year exp

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let Data = [];
 const maxsal = document.getElementById('Max_sal')
 const result = document.getElementById('result')
+const exp = document.getElementById('experience')
 maxsal.addEventListener('click',()=>{
     fetch('data.json')
 .then((res)=>res.json())
@@ -22,5 +23,34 @@ maxsal.addEventListener('click',()=>{
     result.innerHTML = output;
 })
 
+})
+
+exp.addEventListener('click',()=>{
+    fetch('data.json')
+    .then((res)=>res.json())
+    .then((data)=>{
+        let bonus = '';
+        let output = '';
+        Data = data;
+        Data.sort(function(a,b){
+            return b.salary-a.salary;
+        })
+        
+        Data.forEach(function(user){
+            bonus = user.salary * user.experience * 0.10;
+            if(user.experience>3){
+                output += `
+                <ul>
+                <li>id: ${user.id}</li>
+                <li>Name: ${user.name}</li>
+                <li>Experience: ${user.experience}</li>
+                <li>bonus: ${bonus}</li>
+                </ul>
+                `
+            }
+        })
+        result.innerHTML = output;
+
+    })
 })
 
