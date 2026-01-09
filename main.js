db973666b691755576f3b3b33750d42c7506a397
vatsakshat02 <vatsakshat02@gmail.com>
2026-01-09 14:16:07 +0530

    feat:sorted on the basis of salary

---
+++ main.js
@@ -0,0 +1,24 @@
+let Data = [];
+const maxsal = document.getElementById('Max_sal')
+const result = document.getElementById('result')
+
+fetch('data.json')
+.then((res)=>res.json())
+.then((data)=>{
+    let output = "";
+    Data = data;
+    Data.sort(function(a,b){
+        return b.salary - a.salary;
+    })
+    data.forEach(function(user) {
+        output += `
+        <ul>
+        <li> ID: ${user.id}</li>
+        <li> Name: ${user.name}</li>
+        <li> Salary: ${user.salary}</li>
+        </ul>
+        `
+    });
+    result.innerHTML = output;
+})
+
